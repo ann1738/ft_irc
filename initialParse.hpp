4f825b598f75136f8167788f5b9bb0bd7bdf8fad
ann <anasr@student.42abudhabi.ae>
2022-12-19 08:09:20 +0400

    Add initalParse class header file

---
+++ initialParse.hpp
@@ -0,0 +1,23 @@
+#ifndef INITIALPARSE_HPP
+# define INITIALPARSE_HPP
+
+class initialParse
+{
+private:
+	/* data */
+public:
+	initialParse(/* args */);
+	~initialParse();
+};
+
+initialParse::initialParse(int argc, char** argv)
+{
+	
+}
+
+initialParse::~initialParse()
+{
+}
+
+
+#endif
