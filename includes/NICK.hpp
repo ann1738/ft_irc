e0b4bd95a186ce1464e79eb8e12245d0fb3d68fc
gkintana <81775867+gkintana@users.noreply.github.com>
2022-12-25 15:32:31 +0400

    move NICK helper functions to private

--- includes/NICK.hpp
+++ includes/NICK.hpp
@@ -10,10 +10,7 @@ using namespace std;
 
 class NICK {
 
-	public:
-		NICK();
-		~NICK();
-
+	private:
 		vector<string> parseMessage(char* buffer);
 		int getUserIndex(vector<user> users, int fd);
 		bool isNicknameValid(string nickname);
@@ -21,6 +18,11 @@ class NICK {
 		void sendNumericResponse(int fd, int error_type, vector<string> nickname);
 		string buildResponse(string nickname, string new_nickname);
 		void changeNickname(user& user, string new_nickname);
+
+	public:
+		NICK();
+		~NICK();
+
 		void doNickCommand(vector<user>& users, int fd, char* buffer);
 
 };
