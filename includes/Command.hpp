da0798f871faef8f716d54010be9afd28e8c2d77
aalsuwai <aalsuwai@student.42abudhabi.ae>
2022-12-27 11:39:07 +0400

    changed the file name from Command.cpp to command.cpp

--- includes/Command.hpp
+++ includes/Command.hpp
@@ -2,15 +2,27 @@
 # define COMMAND_HPP
 
 #include <iostream>
+#include "user.hpp"
 
 struct command {
+
+private:
+	std::string	cmd_type;
+	std::string	parameters;
+	user*		client;
+
+	void	setCmdType(std::string s);
+	void	setParameters(std::string p);
+	void	setClient(user& c);
 
 public:
 	command();
 
-	std::string	cmd_type;
-	std::string	parameters;
-	std::string	remaining;
+	std::string	getCmdType() const;
+	std::string	getParameters() const;
+	user&		getClient() const;
+
+	friend class commandParse;
 
 	~command();
 
--- srcs/Command.cpp
+++ srcs/Command.cpp
@@ -1,5 +1,30 @@
 #include "command.hpp"
 
-command::command(): cmd_type(), parameters(), remaining() {}
+command::command(): cmd_type(), parameters(), client() {}
+
+std::string	command::getCmdType() const{
+	return (this->cmd_type);
+}
+
+std::string	command::getParameters() const{
+	return (this->parameters);
+}
+
+user&	command::getClient() const{
+	return (*this->client);
+}
+
+void	command::setCmdType(std::string t){
+	this->cmd_type = t;
+}
+
+void	command::setParameters(std::string p){
+	this->parameters = p;
+}
+
+void	command::setClient(user& c){
+	this->client = &c;
+}
+
 
 command::~command() {}
