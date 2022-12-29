fd1ff50c1c7fa05533c824f6bba561a1eef077e4
aalsuwai <aalsuwai@student.42abudhabi.ae>
2022-12-29 09:45:34 +0400

    use string.find_lost_of() instead of string.find() in commandParse

--- srcs/commandParse.cpp
+++ srcs/commandParse.cpp
@@ -3,14 +3,14 @@
 commandParse::commandParse(): cmd() {}
 
 size_t	commandParse::extractCmdType(const string &buff) {
-	size_t	end = (buff.find(" ") < buff.find("/r/n"))? buff.find(" "): buff.find("/r/n");
+	size_t	end = (buff.find(" ") < buff.find_last_of("/r/n"))? buff.find(" "): buff.find_last_of("/r/n");
 
 	this->cmd.setCmdType(buff.substr(0, end));
 	return (end);
 }
 
 size_t	commandParse::extractParameters(const string &buff, size_t start) {
-	size_t	end = buff.find("/r/n");
+	size_t	end = buff.find_last_of("/r/n");
 
 	this->cmd.setParameters(buff.substr(start, (end - start)));
 	return (end);
@@ -18,7 +18,7 @@ size_t	commandParse::extractParameters(const string &buff, size_t start) {
 
 void	commandParse::parse(const string &buff, user& u) {
 	size_t	i = extractCmdType(buff);
-	if (i != buff.find("/r/n"))
+	if (i != buff.find_last_of("/r/n"))
 		i = extractParameters(buff, (i + 1));
 	this->cmd.setClient(u);
 }
