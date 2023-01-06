b87e686ac96dc1edd1848444d086cb5771d91610
gkintana <81775867+gkintana@users.noreply.github.com>
2023-01-06 22:38:14 +0400

    create new branch to handle signals & PING

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
-#include "includes/initialParse.hpp"
-#include "includes/server.hpp"
+#include "initialParse.hpp"
+#include "server.hpp"
+#include <signal.h>
+
+void signalHandler(int sig) {
+	(void)sig;
+	throw std::runtime_error("\nWeBareBears IRC Server has been terminated.");
+}
 
 int main(int argc, char **argv)
 {
@@ -7,5 +13,11 @@ int main(int argc, char **argv)
 	init.parse(argc, argv);
 
 	std::cout << "Parsing done" << std::endl;
-	server ircserv(init.getPort());
+	signal(SIGINT, signalHandler);
+	signal(SIGQUIT, signalHandler);
+	try {
+		server ircserv(init.getPort());
+	} catch (std::exception &e) {
+		cout << e.what() << endl;
+	}
 }
